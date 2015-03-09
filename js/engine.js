01c17e2d9f91f4066c356d6a2eedba4bd5bb29a6
Edmond S.Nadane <edmond.snadane@hotmail.fr>
2015-03-09 08:59:05 +0100

    Commit debuggage score. ( en cours)

--- js/engine.js
+++ js/engine.js
@@ -204,6 +204,20 @@ function animate() {
     alert("You win !\n" + "votre score est de : " + pacman.score);
 	menuMusic.pause();
 	victoriousSong.play();
+	var scores = optionsData.loadScores();
+				var scoreSaved = false;
+				for (var i = 0; i < scores.scores.length ; i++){
+
+					if((eval(scores.scores[i].mount) < eval(pacman.score)) && scoreSaved===false ){
+
+						scores.scores[i].mount = eval(pacman.score);
+						scores.scores[i].nick = optionsData.loadNickName();
+						optionsData.saveScores(scores);
+						scoreSaved = true;
+
+					}	
+					
+				}
     window.location.reload();
   } else if (pacman.life == 0) {
     // Loose game
